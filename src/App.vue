04237fd987f24e41101db85cc222eab3508999e7
venu Madhava Rao <venumrao@gmail.com>
2025-08-16 20:29:54 -0700

    hello world using vue 3 and typescript

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<!-- src/App.vue -->
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const message = ref("Hello World from Vue 3 + TypeScript!");
+</script>
